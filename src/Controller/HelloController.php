53d3b21e1bcfbe8253806ba2e1c565e3fa5f2971
Bharat V <bindian0509@gmail.com>
2023-07-05 23:59:59 +0530

    Internal Routing w/ Controllers

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -3,8 +3,18 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
 class HelloController {
+
+    private array $words = ["Am", "I ", "a", "computer?"];
+    #[Route('/', name: 'app_index')]
     public function index() : Response {
-        return new Response('Hello World!');
+        return new Response(implode(" ", $this->words));
+    }
+
+    #[Route('/messages/{id}', name: 'app_show')]
+    public function showOne($id) : Response {
+        return new Response($this->words[$id]);
     }
 }
