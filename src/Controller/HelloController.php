5aa30bf391759d59180073e2127bbc6034fd7899
Bharat V <bindian0509@gmail.com>
2023-07-05 23:59:59 +0530

    Installation of Twig

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -7,13 +7,16 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController {
 
+
+
     private array $words = ["Am", "I ", "a", "computer?"];
+
     #[Route('/', name: 'app_index')]
     public function index() : Response {
-        return new Response(implode(" ", $this->words));
+        return new Response("<h1>".implode(" : ", $this->words)."</h1>");
     }
 
-    #[Route('/messages/{id}', name: 'app_show')]
+    #[Route('/messages/{id}', name: 'app_show', requirements: ['id' => '\d+'])]
     public function showOne($id) : Response {
         return new Response($this->words[$id]);
     }
